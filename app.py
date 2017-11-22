6db3267f94138b2d33105813d9871fe26da4ca31
Abhi <you@example.com>
2017-11-22 00:47:37 -0800

    dnaskn

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def worker_serve_ding(body):
 
 
 def post_message(message, noOfPeople, senderEmail):
-	emailList = ['abhiram.304@gmail.com']
+	emailList = ['abhiramreddy.salammagari@sjsu.edu']
 	for x in range(0, noOfPeople):
 		if emailList[x] != senderEmail:
 			api.messages.create(toPersonEmail=emailList[x], text=message)
